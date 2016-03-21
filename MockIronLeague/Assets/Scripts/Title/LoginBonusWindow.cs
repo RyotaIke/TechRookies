e25a009b11d8700115c6e4f8b61845e74ddd591f
kasahara_shiho <kasahara_shiho@sumzap.co.jp>
2016-03-22 01:11:39 +0900

    ルーム入るときの挙動修正

--- MockIronLeague/Assets/Scripts/Title/LoginBonusWindow.cs
+++ MockIronLeague/Assets/Scripts/Title/LoginBonusWindow.cs
@@ -20,8 +20,21 @@ public class LoginBonusWindow : SingletonMonoBehaviour<LoginBonusWindow> {
 
 	public void DeactivateLoginBonusWindow()
 	{
+		StartCoroutine (UpdateLastLogin ());
 		bg.enabled = false;
 		loginBonusWindow.SetActive (false);
+	}
+
+	private IEnumerator UpdateLastLogin()
+	{
+		WWWForm wwwForm = new WWWForm();
+		wwwForm.AddField("keyword", "data");//不正接続防止用キーワード
+
+		// get_debug_index
+		string url = ApiList.ApiList.BASE_API_URL + ApiList.ApiList.UPDATE_LAST_LOGIN + "/" + TitleManager.Instance.TerminalId;
+		WWW result = new WWW(url, wwwForm);
+		// レスポンスを待つ
+		yield return result;
 		TitleManager.Instance.ActivateStartBtn ();
 	}
 
