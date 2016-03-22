9a0b2b1182f598a1888d14a3cefe1972ac7b6866
ike_ryota <ike_ryota@cyberagent.co.jp>
2016-03-22 10:17:25 +0900

    ランダムでアイテム or ブロックを出すprefab作成

---
+++ MockIronLeague/Assets/PhotonCoinController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhotonCoinController : MonoBehaviour {
+
+	void Awake()
+	{
+		this.gameObject.transform.SetParent(GameObject.Find("Stage").transform);
+		this.gameObject.transform.localScale = new Vector3 (0.017f, 0.0017f, 1f);
+	}
+}
---
+++ MockIronLeague/Assets/RandomSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSpawner : MonoBehaviour {
+
+	public GameObject[] spawnObjectList;
+
+	// Use this for initialization
+	void Start () {
+		spawnObject ();
+	}
+	
+	/// <summary>
+	/// spawnObjectListの中からランダムで１つ選んで設置する
+	/// </summary>
+	public void spawnObject()
+	{
+		if (PhotonNetwork.isMasterClient) {
+			PhotonNetwork.Instantiate (
+				"Prefabs/" + spawnObjectList [Random.Range (0, spawnObjectList.Length)].name,
+				gameObject.transform.position,
+				Quaternion.identity,
+				0
+			);
+		}
+	}
+}
