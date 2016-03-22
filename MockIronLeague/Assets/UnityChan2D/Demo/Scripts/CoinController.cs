66c75660d6706eab941b469f9421253906eb10b5
kasahara_shiho <kasahara_shiho@sumzap.co.jp>
2016-03-22 14:56:54 +0900

    Playerが取得できないところの修正

--- MockIronLeague/Assets/UnityChan2D/Demo/Scripts/CoinController.cs
+++ MockIronLeague/Assets/UnityChan2D/Demo/Scripts/CoinController.cs
@@ -6,9 +6,9 @@ public class CoinController : MonoBehaviour
 	void OnCollisionEnter2D(Collision2D other)
     {
 		if (LayerMask.LayerToName (other.gameObject.layer) == "Player") {
-			if (other.gameObject.name == "Player_1") {
+			if (other.gameObject.name == "MainPlayer_1") {
 				other.gameObject.GetComponent<PlayerManager> ().OnPlayer1GetCoin ();   
-			} else if (other.gameObject.name == "Player_2") {
+			} else if (other.gameObject.name == "MainPlayer_2") {
 				other.gameObject.GetComponent<PlayerManager> ().OnPlayer3GetCoin ();   
 			}
 			Destroy(gameObject);
