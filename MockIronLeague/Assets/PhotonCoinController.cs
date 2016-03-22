e9164ee5bccd0494b5e4a92230330800e50a7754
ike_ryota <ike_ryota@cyberagent.co.jp>
2016-03-22 16:55:15 +0900

    コインのサイズ調整

--- MockIronLeague/Assets/PhotonCoinController.cs
+++ MockIronLeague/Assets/PhotonCoinController.cs
@@ -6,6 +6,6 @@ public class PhotonCoinController : MonoBehaviour {
 	void Awake()
 	{
 		this.gameObject.transform.SetParent(GameObject.Find("Stage").transform);
-		this.gameObject.transform.localScale = new Vector3 (0.017f, 0.0017f, 1f);
+		this.gameObject.transform.localScale = new Vector3 (0.085f, 0.0085f, 1f);
 	}
 }
